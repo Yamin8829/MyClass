67b5123202bfeb7a67907219d793681c6ab1f4eb
Yamin Mhmd <yaminra88@gmail.com>
2021-12-02 01:10:37 +0700

    nambah poto galery

--- js/templatemo-scripts.js
+++ js/templatemo-scripts.js
@@ -60,7 +60,7 @@ $(document).ready(function() {
   });
 
   // Image Pop Up
-  $(".tm-gallery").magnificPopup({
+  $(".grid-gallery").magnificPopup({
     delegate: "a", // child items selector, by clicking on it popup will open
     type: "image",
     gallery: { enabled: true }
